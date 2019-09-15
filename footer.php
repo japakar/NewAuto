cd89e5594eaeddeddbf9e67759743beebb5af430
japakar <39498837+japakar@users.noreply.github.com>
2019-09-15 13:36:25 -0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,7 @@
+		<div class="w-100 fixed-bottom  container-fluid SecondLayer">
+			<div class="row justify-content-center">
+				<p>Powered by some Autofaucet Script on GitHub</p>
+			</div>
+		</div>
+	</body>
+</html>
